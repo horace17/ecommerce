ee548430662cab13e50480e6ec0e02a71781f1c7
horace <hocedrix2010@gmail.com>
2024-12-30 11:40:58 +0300

    slider working

--- commerce/models.py
+++ commerce/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Slider(models.Model):
+    slider_title = models.CharField(max_length=100)
+    slider_button = models.CharField(max_length=100)
+    slider_image = models.ImageField(upload_to='slides/')
+
+    def __str__(self):
+        return self.slider_title
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -70,14 +70,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecommerce',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -117,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
